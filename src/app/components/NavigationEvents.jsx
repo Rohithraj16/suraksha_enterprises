520210e32d8e913014b2bfe2cd90f25a15effe39
Rohithraj16 <www.rohithraj08@gmail.com>
2025-06-17 18:55:09 +0530

    navigationEvent error solved

--- src/app/components/NavigationEvents.jsx
+++ src/app/components/NavigationEvents.jsx
@@ -1,20 +1,27 @@
-'use client';
+"use client";
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useLoading } from "./LoadingScreen";
 
-import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useLoading } from './LoadingScreen';
+function NavigationEventsInner() {
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const { setIsLoading } = useLoading();
+
+	useEffect(() => {
+		const url = `${pathname}${searchParams ? `?${searchParams}` : ""}`;
+
+		// Set loading to false when route change completes
+		setIsLoading(false);
+	}, [pathname, searchParams, setIsLoading]);
+
+	return null;
+}
 
 export function NavigationEvents() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const { setIsLoading } = useLoading();
-
-  useEffect(() => {
-    const url = `${pathname}${searchParams ? `?${searchParams}` : ''}`;
-    
-    // Set loading to false when route change completes
-    setIsLoading(false);
-  }, [pathname, searchParams, setIsLoading]);
-
-  return null;
+	return (
+		<Suspense fallback={null}>
+			<NavigationEventsInner />
+		</Suspense>
+	);
 }
